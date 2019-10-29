6375fdf7005f986f89c71a86d85ace1a7ea0a081
Koopafan75 <57157585+Koopafan75@users.noreply.github.com>
2019-10-29 18:07:35 -0400

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,27 @@
+// declare global variables for the end date on the timer
+var endDate = new Date("Jan 7, 2020 15:00:00").getTime();
+
+// declare a global variable for the timer, referencing a function, which is executed every second
+var timer = setInterval(timer() {
+	// declare local variables for the current time and the relative time
+	let now = new Date().getTime();
+	let t = endDate - now;
+	
+	// interpret the relative time to get values for the timer
+	if (t >= 0) {
+		let days = Math.floor(t / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+		let secs = Math.floor((t % (1000 * 60)) / 1000);
+		
+		document.getElementById("timer-days").innerHTML = days + "<span class='label'>DAY{S}</span>";
+		document.getElementById("timer-hours").innerHTML = ("0" + hours).slice(-2) + "<span class='label'>HR(S)</span>";
+		document.getElementById("timer-mins").innerHTML = ("0" + mins).slice(-2) + "<span class='label'>MIN(S)</span>";
+		document.getElementById("timer-secs").innerHTML = ("0" + secs).slice(-2) + "<span class='label'>SEC(S)</span>";
+	} else {
+		// Looking through my code? Sneaky. I'd never give anything away this easily...
+		document.getElementById("timer").innerHTML = "Everything is about to change";
+	}
+	
+	
+}, 1000);
